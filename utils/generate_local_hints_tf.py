a6481c6cbef6af2e53db5ad72608936c684b2866
ivanganev <0ivanganev0@gmail.com>
2020-08-17 04:32:56 +0300

    Disaster local hints

--- utils/generate_local_hints_tf.py
+++ utils/generate_local_hints_tf.py
@@ -12,42 +12,32 @@ class LocalHintsGenerator():
         self.full_image_prob = 1 / 100
 
     def get_patch(self, ab, x, y):
-        size = np.random.randint(np.min((5, x + 1, y + 1, self.h - x, self.w - y)))
-        mask = np.zeros((256, 256, 2))
+        size = 5
 
-        mask[x, y, :] = 1
-        mask_tf = tf.convert_to_tensor(mask, tf.float32)
-
+        mask = tf.sparse.SparseTensor(indices=[[x, y, 0], [x, y, 1]], values=[1, 1], dense_shape=[self.h, self.w, 2])
+        mask = tf.sparse.to_dense(mask)
+        mask = tf.cast(mask, tf.float32)
         mean_ab = tf.math.reduce_mean(ab[(x - size):(x + size + 1), (y - size):(y + size + 1), :], axis=(0, 1))
 
-        result = tf.multiply(mask_tf, mean_ab)
+        result = tf.multiply(mask, mean_ab)
 
         return result
 
-        # print(tf.math.reduce_mean(ab[(x - size):(x + size + 1), (y - size):(y + size + 1), :], axis=(0, 1)))
-        #
-        # patch = tf.where(mask_tf, tf.math.reduce_mean(ab[(x - size):(x + size + 1), (y - size):(y + size + 1), :], axis=(0, 1)), tf.zeros_like(mask_tf))
-        # return patch
-        # return tf.math.reduce_mean(ab[(x - size):(x + size + 1), (y - size):(y + size + 1), :], axis=(0, 1))
-
     def generate_local_hints(self, ab):
-        if np.random.random() < self.full_image_prob:
-            return tf.concat([tf.ones([self.h, self.w, 1]), ab], axis=2)
+        hints = 10
 
-        hints = np.random.geometric(self.prob) - 1  # Should we generate with one less
+        pixel = tf.random.uniform(shape=[hints, 2], minval=5, maxval=self.h - 5, dtype=tf.int64)
+        pixel = tf.sort(pixel)
 
-        mask = np.zeros((self.h, self.w, 1))
-        revealed_ab = tf.zeros_like(ab)
+        mask = tf.sparse.SparseTensor(indices=pixel, values=tf.ones([hints]), dense_shape=[self.h, self.w])
+        mask = tf.sparse.reorder(mask)
+        mask = tf.sparse.to_dense(mask)
+        mask = tf.cast(mask, tf.float32)
+        mask = tf.expand_dims(mask, axis=2)
 
-        while hints > 0:
-            x, y = np.random.multivariate_normal(self.mean, self.cov)
-            if x < 0 or y < 0 or x > self.h or y > self.w:
-                continue
-            x, y = int(x), int(y)
-            mask[x, y, 0] = 1
-            revealed_ab = tf.math.maximum(revealed_ab, self.get_patch(ab, x, y))
-            hints = hints - 1
+        revealed_ab = self.get_patch(ab, pixel[hints - 1, 0], pixel[hints - 1, 1])
 
-        mask_tf = tf.convert_to_tensor(mask, tf.float32)
-        return tf.concat([mask_tf, revealed_ab], axis=2)
+        for i in range(hints - 1):
+            revealed_ab = tf.add(revealed_ab, self.get_patch(ab, pixel[i, 0], pixel[i, 1]))
 
+        return tf.concat([mask, revealed_ab], axis=2)
